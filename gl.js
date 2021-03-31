e35d3240a932fa96601599e734b7e86b9522ef80
Clément Corbin <corbin.clement@gmail.com>
2021-03-31 21:29:19 +0200

    unsatisfying oscillation

--- gl.js
+++ gl.js
@@ -27,7 +27,7 @@ class Gl {
       this.textures.push(this.createTexFromImage(image));
     });
   }
-  render(time) {
+  render(time=0) {
     Promise.all(this.textures).then(textures => {
       textures.forEach(tex => {
         tex.position = tex.img.getBoundingClientRect();
@@ -35,9 +35,11 @@ class Gl {
           tex.position.width,
           tex.position.height,
           tex.position.left,
-          tex.position.top);
+          tex.position.top,
+          time);
       });
     });
+    requestAnimationFrame((t) => { this.render(t) });
   }
   resize() {
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
@@ -55,23 +57,19 @@ class Gl {
   trackMouse(e) {
     this.mouse.x = e.clientX;
     this.mouse.y = e.clientY;
-    this.render();
-    //~ this.init();
+    //~ this.render();
   }
   initBuffers() {
     this.positionLocation = this.gl.getAttribLocation(this.program, "a_position");
     this.texcoordLocation = this.gl.getAttribLocation(this.program, "a_texCoord");
-    //~ this.timeLoc = this.gl.getUniformLocation(this.program, "u_time");
-    //~ this.mouseCoordLocation = this.gl.getUniformLocation(this.program, "u_mouseCoord");
     this.matrixLocation = this.gl.getUniformLocation(this.program, "u_matrix");
     this.perspectiveLocation = this.gl.getUniformLocation(this.program, "perspectiveMatrix");
     this.textureLocation = this.gl.getUniformLocation(this.program, "u_texture");
+    this.timeLocation = this.gl.getUniformLocation(this.program, "u_time");
 
-    // Create a buffer.
     this.positionBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
 
-    // Put a unit quad in the buffer
     let positions = [
       0, 0,
       0, 1,
@@ -82,11 +80,9 @@ class Gl {
     ];
     this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
 
-    // Create a buffer for texture coords
     this.texcoordBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.texcoordBuffer);
 
-    // Put texcoords in the buffer
     this.texcoords = [
       0, 0,
       0, 1,
@@ -122,7 +118,6 @@ class Gl {
     var tex = this.gl.createTexture();
     this.gl.bindTexture(this.gl.TEXTURE_2D, tex);
    
-    // let's assume all images are not a power of 2
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
@@ -148,12 +143,10 @@ class Gl {
       });
     }
   }
-  drawImage(tex, texWidth, texHeight, dstX, dstY) {
+  drawImage(tex, texWidth, texHeight, dstX, dstY,time) {
     this.gl.bindTexture(this.gl.TEXTURE_2D, tex);
-    // Tell WebGL to use our shader program pair
     this.gl.useProgram(this.program);
    
-    // Setup the attributes to pull data from our buffers
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
     this.gl.enableVertexAttribArray(this.positionLocation);
     this.gl.vertexAttribPointer(this.positionLocation, 2, this.gl.FLOAT, false, 0, 0);
@@ -161,19 +154,15 @@ class Gl {
     this.gl.enableVertexAttribArray(this.texcoordLocation);
     this.gl.vertexAttribPointer(this.texcoordLocation, 2, this.gl.FLOAT, false, 0, 0);
    
-    // this matrix will convert from pixels to clip space
     let matrix = m4.orthographic(0, this.width, this.height, 0, -1, 1);
    
-    // this matrix will translate our quad to dstX, dstY
     matrix = m4.translate(matrix, dstX, dstY, 0);
    
-    // this matrix will scale our 1 unit quad
-    // from 1 unit to texWidth, texHeight units
     matrix = m4.scale(matrix, texWidth, texHeight, 1);
    
-    // Set the matrices.
     this.gl.uniformMatrix4fv(this.matrixLocation, false, matrix);
-    
+    this.gl.uniform1f(this.timeLocation, time * 0.001);
+
     let mouse = {
       x0: ((this.width/2 - this.mouse.x) > 0) ? (this.mouse.x / (this.width/2))*-1 : -1,
       x1: ((this.width/2 - this.mouse.x) < 0) ? 1-((this.mouse.x / (this.width/2))-1) : 1,
@@ -182,19 +171,7 @@ class Gl {
     };
     let pmatrix = m4.orthographic(mouse.x0, mouse.x1, mouse.y0, mouse.y1, -1, 1);
     this.gl.uniformMatrix4fv(this.perspectiveLocation, false, pmatrix);
-
-    //~ let pmatrix = m4.perspective(2, this.width / this.height, -1, 1000);
-    //~ console.log(pmatrix);
-    //~ let pmatrix = new Float32Array(
-      //~ [0, 0, 0, 0,
-       //~ 0, 0, 0, 0,
-       //~ 0, 0, 0, 0,
-       //~ 0, 0, 0, 0]);
-   
-    // Tell the shader to get the texture from texture unit 0
     this.gl.uniform1i(this.textureLocation, 0);
-   
-    // draw the quad (2 triangles, 6 vertices)
     this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
   }
   mkView(x,y,width,height,img) {
@@ -230,41 +207,26 @@ let vertex = `precision mediump float;
 attribute vec4 a_position;
 attribute vec2 a_texCoord;
 
-//~ uniform vec2 u_mouseCoord;
-//~ uniform float u_time; 
 uniform mat4 u_matrix;
+uniform float u_time;
 uniform mat4 perspectiveMatrix;
  
 varying vec2 v_texCoord;
 
-//~ precision mediump float;
-//~ attribute vec2 position;
-//~ uniform mat4 transformMatrix;
-//~ uniform mat4 perspectiveMatrix;
-//~ varying vec2 texcoords;
-
-
 void main() {
-  //~ float oscillation = 0.0;
-  //~ float amplitude = 0.2;
-  //~ float frequence = 4.0;
-  //~ float angle = (u_time + a_position.x) * frequence;
+  float oscillation = 1.0;
+  float frequence = 4.0;
+  float amplitude = 0.2;
 
-  //~ oscillation +=  sin(angle) * amplitude;
-  //~ oscillation +=  1.0 + u_time;
+  float angle = (u_time + a_position.x) * frequence;
+  oscillation +=  sin(angle) * amplitude;
 
-    //~ gl_Position = perspectiveMatrix * u_matrix * vec4(a_position, oscillation, 1.0);
-   gl_Position = u_matrix * a_position * perspectiveMatrix;
-   //~ gl_Position = u_matrix * vec4(a_position.x, a_position.y, oscillation, 1.0) * perspectiveMatrix;
-   //~ gl_Position = u_matrix * vec4(a_position.x, a_position.y, 1.0, oscillation);
-   v_texCoord = a_texCoord;
+  gl_Position = u_matrix * a_position * perspectiveMatrix * vec4(1.0, 1.0, 1.0, oscillation);;
+  v_texCoord = a_texCoord;
 }`;
 let fragment = `precision mediump float;
-
-// our texture
 uniform sampler2D u_image;
 
-// the texCoords passed in from the vertex shader.
 varying vec2 v_texCoord;
 
 void main() {
