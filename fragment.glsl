83d22b4cf75098462f9415ffe6d4d2d9e9a322de
Clément Corbin <corbin.clement@gmail.com>
2021-04-01 16:34:54 +0200

    simplex noise

---
+++ fragment.glsl
@@ -0,0 +1,15 @@
+precision mediump float;
+uniform sampler2D u_image;
+
+varying vec2 v_texCoord;
+varying float v_wave;
+
+void main() {
+  float wave = v_wave * .2;
+  float r = texture2D(u_image, v_texCoord).r;
+  float g = texture2D(u_image, v_texCoord).g;
+  float b = texture2D(u_image, v_texCoord + wave).b;
+  vec3 texture = vec3(r, g, b);
+  gl_FragColor = vec4(texture, 1.);
+   //~ gl_FragColor = texture2D(u_image, v_texCoord);
+}
